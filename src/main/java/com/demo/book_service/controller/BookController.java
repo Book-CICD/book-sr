1c594b1431d85280bc2eaf84a6a8808e0018b12a
SandeepRamaram7 <sandeepramaram777@gmail.com>
2024-10-28 14:37:00 +0530

    first commit

---
+++ src/main/java/com/demo/book_service/controller/BookController.java
@@ -0,0 +1,48 @@
+package com.demo.book_service.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.book_service.dao.BookRepository;
+import com.demo.book_service.model.Book;
+
+@RestController
+@RequestMapping("/api/book")
+public class BookController {
+
+	@Autowired
+	BookRepository bookRepo;
+	
+	@GetMapping("/books")
+	public List<Book> getAllBooks() {
+		return this.bookRepo.findAll();
+	}
+	
+	@GetMapping("/books/{bid}")
+	public Book getABooks(@PathVariable int bid) {
+		return this.bookRepo.findById(bid).get();
+	}
+	
+	@PostMapping("/books")
+	public Book addBook(@RequestBody Book newBook) {
+		return this.bookRepo.save(newBook);
+	}
+	
+	@DeleteMapping("/books/{bid}")
+	public void deleteBook(@PathVariable int bid) {
+		this.bookRepo.deleteById(bid);
+	}
+
+	@GetMapping("/books/authur/{authurId}")
+	public List<Book> getBooksByAuthur(@PathVariable String authurId) {
+		return this.bookRepo.findByBookAuthurId(authurId);
+	}
+}
---
+++ src/main/java/com/demo/book_service/model/Book.java
@@ -0,0 +1,27 @@
+package com.demo.book_service.model;
+
+import java.time.LocalDate;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+@Builder
+@Document(collection = "books")
+public class Book {
+	@Id
+	private int bookId;
+	private String bookTitle;
+	private String bookPrice;
+	private LocalDate bookPublished;
+	private String bookImageUrl;
+	private String bookAuthurId;
+}
